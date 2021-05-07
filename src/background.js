62791c55ce1c5bbf0355ed27d9855646a3cb1b72
Alexander <sashok935@gmail.com>
2021-05-08 00:48:52 +0300

    Port extension to v3, add badge and website messaging

--- src/background.js
+++ src/background.js
@@ -1,20 +1,89 @@
-chrome.browserAction.onClicked.addListener((tab) => {
-  const toolsUrls = ['holodex.net'];
-  if (toolsUrls.some(url => tab.url.includes(url))) {
+const ALARM = {
+  REFRESH_BADGE: 'refreshBadge',
+  REFRESH_FAVORITES: 'refreshFavorites',
+};
+const MESSAGE = {
+  TOKEN: 'token',
+  FAVORITES: 'favorites',
+};
+const siteUrl = 'https://holodex.net/';
+const apiUrl = `${siteUrl}api/v2`;
+
+const cacheFavorites = async (jwt) => {
+  const response = await fetch(`${apiUrl}/users/favorites`, {
+    headers: { Authorization: `Bearer ${jwt}` },
+  });
+  if (response.ok) {
+    const json = await response.json();
+    const favorites = json.map((f) => f.id);
+    chrome.storage.local.set({ favorites });
+  }
+};
+
+const updateBadge = async (jwt) => {
+  const { favorites } = await new Promise((resolve) => chrome.storage.local.get({ favorites: [] }, resolve));
+  const response = await fetch(`${apiUrl}/users/live?channels=${favorites.join(',')}`, {
+    headers: { Authorization: `Bearer ${jwt}` },
+  });
+  if (response.ok) {
+    const json = await response.json();
+    chrome.action.setBadgeText({ text: json.length.toString() || null });
+  }
+  return null;
+};
+
+chrome.action.onClicked.addListener(async (tab) => {
+  const newTab = 'chrome://newtab/';
+
+  if (tab.url.includes(siteUrl)) {
     return;
   }
-  const toolsUrl = 'https://holodex.net/';
-  const newTab = 'chrome://newtab/';
 
   const query = tab.url.split('?')[1];
   const params = new URLSearchParams(query);
-  const ytVideoId = params.has('v') && params.get('v');
+  const ytVideoId = params.get('v');
 
-  ytVideoId || tab.url === newTab
-    ? chrome.tabs.update(tab.id, {
-      url: ytVideoId ? `${toolsUrl}multiview/AATY${ytVideoId}` : toolsUrl
-    })
-    : chrome.tabs.create({
-      url: toolsUrl,
+  if (ytVideoId || tab.url === newTab) {
+    chrome.tabs.update(tab.id, { url: ytVideoId ? `${siteUrl}multiview/AATY${ytVideoId}` : siteUrl });
+  } else {
+    chrome.tabs.create({ url: siteUrl });
+  }
+});
+
+chrome.runtime.onMessageExternal.addListener((request, sender) => {
+  if (!sender.url.includes('holodex.net')) {
+    return;
+  }
+
+  if (request.message === MESSAGE.TOKEN) {
+    const token = request.token;
+    chrome.storage.local.set({ token }, async () => {
+      if (!token) {
+        return;
+      }
+      await cacheFavorites(token);
+      await updateBadge(token);
     });
+  }
+
+  if (request.message === MESSAGE.FAVORITES) {
+    chrome.storage.local.get({ token: null }, ({ token }) => {
+      token && cacheFavorites(token);
+    });
+  }
+});
+
+chrome.alarms.create(ALARM.REFRESH_BADGE, { periodInMinutes: 1 });
+chrome.alarms.create(ALARM.REFRESH_FAVORITES, { periodInMinutes: 60 });
+chrome.alarms.onAlarm.addListener((alarm) => {
+  chrome.storage.local.get({ token: null }, ({ token }) => {
+    switch (token && alarm.name) {
+      case ALARM.REFRESH_BADGE:
+        return updateBadge(token);
+      case ALARM.REFRESH_FAVORITES:
+        return cacheFavorites(token);
+      default:
+        return;
+    }
+  });
 });
